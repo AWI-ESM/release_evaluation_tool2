63fa5420be37c002098e1170f876dcc0b8070d95
Jan Streffing <jan.streffing@awi.de>
2025-02-18 15:40:43 +0100

    8 and 9 work

--- scripts/part8_t2m_vs_era5.py
+++ scripts/part8_t2m_vs_era5.py
@@ -1,115 +1,15 @@
-#Name of model release
-model_version  = 'TCo1279-DART'
-
-#Spinup
-spinup_path    = '/scratch/awicm3/TCo1279-DART-1950/outdata/'
-spinup_name    = model_version+'_spinup'
-spinup_start   = 1955
-spinup_end     = 1969
-
-#Preindustrial Control
-pi_ctrl_path   = '/scratch/awicm3/TCo1279-DART-1950/outdata/'
-pi_ctrl_name   = model_version+'_pi-control'
-pi_ctrl_start  = 1955
-pi_ctrl_end    = 1969
-
-#Historic
-historic_path  = '/scratch/awicm3/TCo1279-DART-2000/outdata/'
-historic_name  = model_version+'_historic'
-historic_start = 2002
-historic_end   = 2009
-
-
-#Misc
-reanalysis             = 'ERA5'
-remap_resolution       = '5124x2560'
-dpi                    = 900
-historic_last25y_start = historic_start
-historic_last25y_end   = historic_end
-
-#Mesh
-mesh_name      = 'DART'
-grid_name      = 'TCo1279'
-meshpath       = '/proj/awi/input/fesom2/dart/'
-mesh_file      = 'dart_griddes_nodes.nc'
-griddes_file   = 'dart_griddes_nodes.nc'
-abg            = [0, 0, 0]
-reference_path = '/proj/awiiccp5/climatologies/'
-reference_name = 'clim'
-reference_years= 1990
-
-observation_path = '/proj/awi/'
-
-
-#Data access and structures
-import pyfesom2 as pf
-import xarray as xr
-from cdo import *   # python version
-cdo = Cdo(cdo='/home/awiiccp2/miniconda3/envs/pyfesom2/bin/cdo')
-from netCDF4 import Dataset
-import numpy as np
-import pandas as pd
-from collections import OrderedDict
-import csv
-
-#Plotting
-import math as ma
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import matplotlib.colors as colors
-from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
-                               AutoMinorLocator)
-from matplotlib.ticker import Locator
-from matplotlib import ticker
-from matplotlib import cm
-import seaborn as sns
-from cartopy import config
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-from cartopy.util import add_cyclic_point
-from mpl_toolkits.basemap import Basemap
-import cmocean as cmo
-from cmocean import cm as cmof
-import matplotlib.pylab as pylab
-import matplotlib.patches as Polygon
-import matplotlib.ticker as mticker
-
-
-#Science
-import math
-from math import sqrt
-from sklearn.metrics import mean_squared_error
-from eofs.standard import Eof
-from eofs.examples import example_data_path
-import shapely
-from scipy import signal
-from scipy.stats import linregress
-from scipy.spatial import cKDTree
-from scipy.interpolate import CloughTocher2DInterpolator, LinearNDInterpolator, NearestNDInterpolator
-
-#Misc
+# Add the parent directory to sys.path and load config
+import sys
 import os
-import warnings
-from tqdm import tqdm
-import logging
-import joblib
-import dask
-from dask.delayed import delayed
-from dask.diagnostics import ProgressBar
-import random as rd
-import time
-import copy as cp
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from config import *
 
-#Fesom related routines
-from set_inputarray  import *
-from sub_fesom_mesh  import * 
-from sub_fesom_data  import * 
-from sub_fesom_moc   import *
-from colormap_c2c    import *
+SCRIPT_NAME = os.path.basename(__file__)  # Get the current script name
 
-tool_path      = os.getcwd()
-out_path       = tool_path+'/output/plot/'+model_version+'/'
+print(SCRIPT_NAME)
+
+# Mark as started
+update_status(SCRIPT_NAME, " Started")
 
 
 mesh = pf.load_mesh(meshpath)
@@ -122,7 +22,7 @@ input_names = [historic_name]
 if reanalysis=='ERA5':
     clim='ERA5'
     clim_var='t2m'
-    climatology_files = ['T2M_yearmean_hr.nc']
+    climatology_files = ['T2M_yearmean.nc']
     title='Near surface (2m) air tempereature vs. ERA5'
     climatology_path = observation_path+'/era5/netcdf/'
 elif reanalysis=='NCEP2':
@@ -134,16 +34,13 @@ elif reanalysis=='NCEP2':
 
 exps=[]
 for year in range(historic_last25y_start, historic_last25y_end + 1):
-    # Loop through each month (1 through 12)
-    for month in range(1, 13):
-        # Append year and month in the format YYYYMM
-        exps.append(year * 100 + month)
+    exps.append(year)
         
 figsize=(6, 4.5)
 ofile = None
-res = [1440, 720]
+res = [360, 180]
 var = ['2t']
-levels = [-5.0,-3.0,-2.0,-1.0,-.6,-.2,.2,.6,1.0,2.0,3.0,5.0]
+levels = [-8.0,-5.0,-3.0,-2.0,-1.0,-.6,-.2,.2,.6,1.0,2.0,3.0,5.0,8.0]
 contour_outline_thickness = 0
 
 # Set number of columns, in case of multiple variables
@@ -172,25 +69,38 @@ def load_parallel(variable,path):
     data1 = cdo.timmean(input="-remapcon,r"+str(res[0])+"x"+str(res[1])+" "+str(path),returnArray=variable)
     return data1
 
+
+
+
+chunk_size = 10  # Process in chunks of 20
+
 data = OrderedDict()
-for exp_path, exp_name  in zip(input_paths, input_names):
+for exp_path, exp_name in zip(input_paths, input_names):
     data[exp_name] = {}
     for v in var:
         datat = []
         t = []
-        temporary = []
-        for exp in tqdm(exps):
 
-            path = exp_path+'/oifs/atm_reduced_1m_'+v+'_1m_'+f'{exp:04d}-{exp:04d}.nc'
-            temporary = dask.delayed(load_parallel)(v,path)
-            t.append(temporary)
+        # Process in chunks
+        for i in range(0, len(exps), chunk_size):
+            chunk = exps[i:i + chunk_size]
+            chunk_t = []
+            
+            for exp in chunk:
+                path = f"{exp_path}/oifs/atm_remapped_1m_{v}_1m_{exp:04d}-{exp:04d}.nc"
+                temporary = dask.delayed(load_parallel)(v, path)
+                chunk_t.append(temporary)
 
-        with ProgressBar():
-            datat = dask.compute(t)
+            with ProgressBar():
+                datat_chunk = dask.compute(*chunk_t, scheduler='threads')
+            
+            datat.extend(datat_chunk)
+
         data[exp_name][v] = np.squeeze(datat)
-        
+
 data_model = OrderedDict()
 data_model_mean = OrderedDict()
+
 
 
 for exp_name in input_names:
@@ -203,13 +113,13 @@ for exp_name in input_names:
 print(np.shape(data_model_mean[exp_name]))
 print(np.shape(data_reanalysis_mean))
 
-lon = np.arange(0, 360, 0.25)
-lat = np.arange(-90, 90, 0.25)
+lon = np.arange(0, 360, 1)
+lat = np.arange(-90, 90, 1)
 data_model_mean[exp_name], lon = add_cyclic_point(data_model_mean[exp_name], coord=lon)
 
 
-lon = np.arange(0, 360, 0.25)
-lat = np.arange(-90, 90, 0.25)
+lon = np.arange(0, 360, 1)
+lat = np.arange(-90, 90, 1)
 data_reanalysis_mean, lon = add_cyclic_point(data_reanalysis_mean, coord=lon)
 
 print(np.shape(data_model_mean[exp_name]))
@@ -222,67 +132,71 @@ rmsdval = sqrt(mean_squared_error(data_model_mean[exp_name],data_reanalysis_mean
 mdval = md(data_model_mean[exp_name],data_reanalysis_mean,wgts)
 
 
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 
-
+# Define figure layout
 nrows, ncol = define_rowscol(input_paths)
-fig, axes = plt.subplots(nrows=nrows, ncols=ncol, figsize=figsize, subplot_kw={'projection': ccrs.Robinson(central_longitude=-160)})
+fig, axes = plt.subplots(nrows=nrows, ncols=ncol, figsize=figsize,
+                         subplot_kw={'projection': ccrs.PlateCarree()})  # Use PlateCarree globally
+
 if isinstance(axes, np.ndarray):
     axes = axes.flatten()
 else:
     axes = [axes]
-i = 0
 
-
-for key in input_names:
-
-    axes[i]=plt.subplot(nrows,ncol,i+1,projection=ccrs.PlateCarree())
-    axes[i].add_feature(cfeature.COASTLINE,zorder=3)
+# Loop through input names and plot data
+for i, exp_name in enumerate(input_names):
+    print(exp_name)
     
-    
-    imf=plt.contourf(lon, lat, data_model_mean[exp_name]-
-                    data_reanalysis_mean, cmap=plt.cm.PuOr_r, 
-                     levels=levels, extend='both',
-                     transform=ccrs.PlateCarree(),zorder=1)
-    line_colors = ['black' for l in imf.levels]
-    imc=plt.contour(lon, lat, data_model_mean[exp_name]-
-                    data_reanalysis_mean, colors=line_colors, 
-                    levels=levels, linewidths=contour_outline_thickness,
-                    transform=ccrs.PlateCarree(),zorder=1)
+    ax = axes[i]
+    ax.add_feature(cfeature.COASTLINE, zorder=3)
 
-    axes[i].set_ylabel('K')
-    axes[i].set_xlabel('Simulation Year')
+    # Contour plot
+    imf = ax.contourf(lon, lat, data_model_mean[exp_name] - data_reanalysis_mean, 
+                       cmap=plt.cm.PuOr_r, levels=levels, extend='both',
+                       transform=ccrs.PlateCarree(), zorder=1)
     
-    axes[i].set_title(title,fontweight="bold")
-    plt.tight_layout() 
-    gl = axes[i].gridlines(crs=ccrs.PlateCarree(), draw_labels=True,
-                  linewidth=1, color='gray', alpha=0.2, linestyle='-')
+    line_colors = ['black' for _ in imf.levels]
+    imc = ax.contour(lon, lat, data_model_mean[exp_name] - data_reanalysis_mean, 
+                     colors=line_colors, levels=levels, linewidths=contour_outline_thickness,
+                     transform=ccrs.PlateCarree(), zorder=1)
 
+    ax.set_ylabel('K')
+    ax.set_xlabel('Simulation Year')
+    ax.set_title(title, fontweight="bold")
+
+    # Gridlines
+    gl = ax.gridlines(draw_labels=True, linewidth=1, color='gray', alpha=0.2, linestyle='-')
     gl.xlabels_bottom = False
 
-    
-    cbar_ax_abs = fig.add_axes([0.15, 0.11, 0.7, 0.05])
-    cbar_ax_abs.tick_params(labelsize=12)
-    cb = fig.colorbar(imf, cax=cbar_ax_abs, orientation='horizontal',ticks=levels)
-    cb.set_label(label="°C", size='14')
-    cb.ax.tick_params(labelsize='12')
-    
-    textrsmd='rmsd='+str(round(rmsdval,3))
-    textbias='bias='+str(round(mdval,3))
+    # Bias & RMSD Text
+    textrsmd = f'rmsd={round(rmsdval, 3)}'
+    textbias = f'bias={round(mdval, 3)}'
     props = dict(boxstyle='round,pad=0.1', facecolor='white', alpha=0.5)
-    axes[i].text(0.02, 0.4, textrsmd, transform=axes[i].transAxes, fontsize=13,
-        verticalalignment='top', bbox=props, zorder=4)
-    axes[i].text(0.02, 0.3, textbias, transform=axes[i].transAxes, fontsize=13,
-        verticalalignment='top', bbox=props, zorder=4)
-    
-    i = i+1
-    
+
+    ax.text(0.02, 0.4, textrsmd, transform=ax.transAxes, fontsize=13,
+            verticalalignment='top', bbox=props, zorder=4)
+    ax.text(0.02, 0.3, textbias, transform=ax.transAxes, fontsize=13,
+            verticalalignment='top', bbox=props, zorder=4)
+
+# Colorbar
+cbar_ax_abs = fig.add_axes([0.15, 0.11, 0.7, 0.05])
+cbar_ax_abs.tick_params(labelsize=12)
+cb = fig.colorbar(imf, cax=cbar_ax_abs, orientation='horizontal', ticks=levels)
+cb.set_label(label="°C", size=14)
+cb.ax.tick_params(labelsize=12)
+
+# Hide every other tick label
 for label in cb.ax.xaxis.get_ticklabels()[::2]:
     label.set_visible(False)
 
-    
-ofile='t2m_vs_'+clim
-    
+# Save figure
+ofile = f't2m_vs_{clim}'
 if ofile is not None:
-    plt.savefig(out_path+ofile, dpi=dpi,bbox_inches='tight')
-    os.system(f'convert {ofile} -trim {ofile}_trimmed.png')
-    os.system(f'mv {ofile}_trimmed.png {ofile}')
+    plt.savefig(out_path + ofile, dpi=dpi, bbox_inches='tight')
+
+# Mark script as completed
+update_status(SCRIPT_NAME, "Completed")
+
